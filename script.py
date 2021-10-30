67738ae2c0b82dcbd1c6c5d05f10be110129f0c9
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-10-30 13:28:33 +0200

    hemos terimado la tarea 1

--- script.py
+++ script.py
@@ -1,2 +1,6 @@
+#tarea 1
 print("Roses are #ff0000 Violets are #0000ff why my code's working I haven't a clue")
-print("lo que quieras")
+#tarea 4
+altura=200
+altura=200+50
+print(altura)
---
+++ tarea 3.py
@@ -0,0 +1,5 @@
+#En el editor codigo, imprima el resultado de 21 + 43, es decir, en el codigo, escriba lo siguiente:
+print(21+43)
+print(142-52)
+print(10*342)
+print(5**2)
